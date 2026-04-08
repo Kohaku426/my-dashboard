c68d14d6a0de6e3bfea407cbb1ea89b40849f54c
Kohaku <kotarohaku0829@email.com>
2026-04-08 22:16:23 +0900

    統合PWAダッシュボード追加（就活+健康管理、ホーム画面対応）

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'my-dashboard-v1';
+const FILES = [
+  '/my-dashboard/',
+  '/my-dashboard/index.html',
+  '/my-dashboard/manifest.json',
+  '/my-dashboard/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    if(res.ok && e.request.method === 'GET') {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+    }
+    return res;
+  })));
+});
